1978a5c710fadbaed4437ce59ba5c9c1754f18e6
Artur <kil_artur@inbox.ru>
2024-06-14 15:53:06 +0300

    Поиск товара по 50 страницам

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ handler = logging.StreamHandler(sys.stdout)
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
-name_and_article = 'кукла 147404230'
+name_and_article = 'кукла 170268597'
 
 name = str(name_and_article.split()[0])
 article = int(name_and_article.split()[-1])
@@ -17,22 +17,6 @@ article = int(name_and_article.split()[-1])
 # Правильный URL для API, возвращающий JSON
 API_URL = 'https://search.wb.ru/exactmatch/ru/common/v5/search'
 
-# Параметры запроса
-params = {
-    'ab_testing': 'false',
-    'appType': '1',
-    'curr': 'rub',
-    'dest': '-369516',
-    'page': '1',
-    'query': name,
-    'resultset': 'catalog',
-    'sort': 'popular',
-    'spp': '30',
-    'suppressSpellcheck': 'false',
-    'uclusters': '0'
-}
-
-# Заголовки запроса
 headers = {
     "Accept": "*/*",
     "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -50,26 +34,43 @@ headers = {
     "x-userid": "131290320"
 }
 
-# Выполнение GET-запроса к API
-response = requests.get(API_URL, headers=headers, params=params)
+# Ограничение в первые 50 страниц
+for number_page in range(1, 51):
+    # Параметры запроса
+    params = {
+        'ab_testing': 'false',
+        'appType': '1',
+        'curr': 'rub',
+        'dest': '-369516',
+        'page': number_page,
+        'query': name,
+        'resultset': 'catalog',
+        'sort': 'popular',
+        'spp': '30',
+        'suppressSpellcheck': 'false',
+        'uclusters': '0'
+    }
 
-# Печать деталей запроса
+    # Выполнение GET-запроса к API
+    response = requests.get(API_URL, headers=headers, params=params)
 
-# Проверка успешности запроса и печать JSON-данных
-if response.status_code == 200:
-    try:
-        json_data = response.json()  # Получаем JSON как словарь
-        # json_data = json.dumps(json_data, indent=4, ensure_ascii=False)
-        for i in range(99):
-            if json_data['data']['products'][i]['id'] == article:
-                print(f"Ваш продукт на {params['page']} странице, {i + 1} месте")
-            # logger.info(json_data['data']['products'][i]['id'])
-        # list_article = []
-        # for number in range(99):
-        #     list_article.append(['data']['products'][number]['id'])
-        # logger.info(list_article)
-    except json.JSONDecodeError:
-        logger.warning('Ответ не является JSON')
-else:
-    logger.error(f'Ошибка: {response.status_code}')
+    # Печать деталей запроса
+
+    # Проверка успешности запроса и печать JSON-данных
+    if response.status_code == 200:
+        try:
+            json_data = response.json()  # Получаем JSON как словарь
+            # json_data = json.dumps(json_data, indent=4, ensure_ascii=False)
+            for i in range(99):
+                if json_data['data']['products'][i]['id'] == article:
+                    print(f"Ваш продукт на {params['page']} странице, {i + 1} месте")
+                # logger.info(json_data['data']['products'][i]['id'])
+            # list_article = []
+            # for number in range(99):
+            #     list_article.append(['data']['products'][number]['id'])
+            # logger.info(list_article)
+        except json.JSONDecodeError:
+            logger.warning('Ответ не является JSON')
+    else:
+        logger.error(f'Ошибка: {response.status_code}')
 
